9b756e369980fc859d540da8fb6590e1cb8395da
Nigel Hagen <thedjskywalker@gmail.com>
2026-02-07 01:46:31 +0100

    chore: use --exclude-glob for FTP exclude patterns

--- upload_ftp.sh
+++ upload_ftp.sh
@@ -3,7 +3,7 @@
 source ftp_credentials.local
 # Exclude .git, ftp creds, and common local files that shouldn't be published
 # Exclude .git, ftp creds, and other local files that shouldn't be published
-EXCLUDES=("--exclude .git" "--exclude .git/*" "--exclude ftp_credentials.local" "--exclude .github" "--exclude .gitignore" "--exclude iptv-detective-backup.bundle" "--exclude .venv" "--exclude '*.pyc'")
+EXCLUDES=("--exclude-glob .git" "--exclude-glob .git/*" "--exclude-glob ftp_credentials.local" "--exclude-glob .github" "--exclude-glob .gitignore" "--exclude-glob iptv-detective-backup.bundle" "--exclude-glob .venv" "--exclude-glob '*.pyc'")
 
 # Mirror options: continue partial uploads, avoid symlink operations and permissions changes, use small parallelism
 MIRROR_OPTS=("--continue" "--no-symlinks" "--no-perms" "--parallel=2" "--verbose")
