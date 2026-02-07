83d9373ffc13967a687941b43af41486060a04d7
Nigel Hagen <thedjskywalker@gmail.com>
2026-02-07 01:43:01 +0100

    chore: exclude .git and ftp_credentials.local from FTP upload

--- upload_ftp.sh
+++ upload_ftp.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# Upload workspace to FTP, excluding local credentials and VCS metadata
 source ftp_credentials.local
-lftp -u $FTP_USERNAME,$FTP_PASSWORD $FTP_HOST << EOF2
-mirror -R . "$FTP_DIR"
+# Exclude .git, ftp creds, and common local files that shouldn't be published
+EXCLUDES=("--exclude .git" "--exclude .git/*" "--exclude ftp_credentials.local" "--exclude .github" "--exclude .gitignore")
+
+lftp -u "$FTP_USERNAME","$FTP_PASSWORD" "$FTP_HOST" << EOF2
+mirror -R . "$FTP_DIR" ${EXCLUDES[*]}
 quit
 EOF2
